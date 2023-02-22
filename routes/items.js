6cfa3a91095208b9222e082ddbe2ee9f1d64243d
Константин Олейник <oks@sto.local>
2023-02-22 13:31:06 +0200

    options, schema, handler

--- routes/items.js
+++ routes/items.js
@@ -1,18 +1,49 @@
 const items = require('../Items');
 
-function itemRoutes(fastify, options, done) {
-  //   Get all items
-  fastify.get('/items', async (req, reply) => {
-    //   reply.send({ test: 'Hello world' });
-    reply.send(items);
-  });
+// Item schema
+const Item = {
+  type: 'object',
+  properties: {
+    id: { type: 'string' },
+    name: { type: 'string' },
+  },
+};
 
-  // Get single item
-  fastify.get('/items/:id', async (req, reply) => {
+// Options for get all items
+const getItemsOpts = {
+  schema: {
+    response: {
+      200: {
+        type: 'array',
+        items: Item,
+      },
+    },
+  },
+  handler: function (req, reply) {
+    reply.send(items);
+  },
+};
+
+// Options for single item
+const getItemOpts = {
+  schema: {
+    response: {
+      200: Item,
+    },
+  },
+  handler: function (req, reply) {
     const { id } = req.params;
     const item = items.find((item) => item.id === id);
     reply.send(item);
-  });
+  },
+};
+
+function itemRoutes(fastify, options, done) {
+  //   Get all items
+  fastify.get('/items', getItemsOpts);
+
+  // Get single item
+  fastify.get('/items/:id', getItemOpts);
 
   done();
 }
